36d60dfadf73b9cc6e986986793f96161cd93d09
NuryeNigusMekonen <nurye.nigus.me@gmail.com>
2025-05-19 21:37:55 +0300

    fix: adjust sys.path to import utils from parent directory

--- app/main.py
+++ app/main.py
@@ -2,7 +2,13 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from utils import load_data, plot_ghi_boxplot
+
 st.set_page_config(page_title="Solar Insights Dashboard", layout="wide")
 st.title("Solar Potential Comparison Dashboard")
 # Sidebar for country selection
