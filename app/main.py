ea6b3ebb646b4224c8ce31c34454738576fe3d87
NuryeNigusMekonen <nurye.nigus.me@gmail.com>
2025-05-20 12:27:42 +0300

    fix: overwrite main.py and utils.py from dashboard-dev

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ from utils import load_data
 # Set wide layout and custom title
 st.set_page_config(page_title=" Solar Insights Dashboard by Nurye Nigus", layout="wide")
 
-#  Sidebar
-st.sidebar.title(" Controls")
+# Sidebar 
+st.sidebar.title(" Control ")
 
 country = st.sidebar.selectbox(" Select Country", ["Benin", "Sierra Leone", "Togo"])
 metric = st.sidebar.radio(" Select Metric", ["GHI", "DNI", "DHI"])
@@ -18,33 +18,8 @@ top_n = st.sidebar.slider(" Show Top N Values(5-50)", min_value=5, max_value=50,
 st.title(" Solar Irradiance Analysis Dashboard done by Nurye Nigus")
 st.markdown("Explore cleaned solar irradiance data across the three contries given")
 
-#  Load Data 
+# Load Data
 df = load_data(country)
-if df.empty:
-    st.error("No data available.")
-    st.stop()
-# Metrics Summary 
-st.subheader(f" Summary Statistics – {country}")
-st.dataframe(df[[metric]].describe().T.round(2))
-# Plot & Table Layout 
-col1, col2 = st.columns([2, 1])
-# Boxplot 
-with col1:
-    st.subheader(f" Boxplot of {metric}")
-    fig, ax = plt.subplots(figsize=(7, 4))
-    sns.boxplot(y=df[metric], color='mediumseagreen', ax=ax)
-    ax.set_ylabel(f"{metric} (W/m²)")
-    st.pyplot(fig)
-# ---- Top N Table ----
-with col2:
-    st.subheader(f" Top {top_n} Highest {metric} Readings")
-    st.dataframe(df[[metric]].sort_values(by=metric, ascending=False).head(top_n).reset_index(drop=True))
-
-
-# ---- Expander for Raw Data Preview ----
-with st.expander(" View Raw Data Sample"):
-    st.dataframe(df.head(20))
-
 
 if df.empty:
     st.error("No data available.")
@@ -74,6 +49,6 @@ with col2:
 with st.expander(" View Raw Data Sample"):
     st.dataframe(df.head(20))
 
-# ---- Footer ----
+#  Footer 
 st.markdown("---")
 st.markdown("Made with by Nurye Nigus | [GitHub](https://github.com/NuryeNigusMekonen)")
--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
+
+
 def load_data(country):
     filename = {
         "Benin": "cleaned_data/benin_cleaned_data.csv",
@@ -15,3 +17,4 @@ def load_data(country):
     except FileNotFoundError:
         st.warning(f" File not found: {filename}")
         return pd.DataFrame()
+
